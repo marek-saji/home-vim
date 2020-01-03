884d2d7fa50d9ae5a657cf3e671600be574703ba
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2020-01-03 15:10:39 +0100

    squash! Pack update script
    Plugin manager

---
+++ plug.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+set -e
+
+PACK=saji
+PACK_DIR="$( cd "$( dirname "$0" )" && pwd -P )/pack/$PACK"
+
+COMMAND="$1"
+if [ "$#" -gt 0 ]
+then
+    shift
+fi
+
+help ()
+{
+    printf "Manage VIM plugins using vim’s packages (in %s).\n" "$PACK_DIR"
+    printf "\n"
+    printf "Usage:\n"
+    printf "  %s add GIT_URL [NAME] [LOAD_POLICY]\n" "$0"
+    printf "  %s upgrade\n" "$0"
+    printf "  %s remove NAME [LOAD_POLICY]\n" "$0"
+    printf "\n"
+    printf "  NAME may be left empty to get it from GIT_URL\n"
+    printf "  LOAD_POLICY is 'start' (default) or 'opt'\n"
+}
+
+add ()
+{
+    URL="$1"
+    NAME="$2"
+    LOAD_POLICY="${3:-start}"
+    if [ -z "$NAME" ]
+    then
+        NAME="$( basename "$URL" .git )"
+    fi
+    set -x
+    git submodule add "$URL" "$PACK_DIR/$LOAD_POLICY/$NAME"
+    git commit -m "plugin: $NAME"
+}
+
+remove ()
+{
+    NAME="$1"
+    LOAD_POLICY="${2:-start}"
+    set -x
+    git submodule deinit "$PACK_DIR/$LOAD_POLICY/$NAME"
+    git rm "$PACK_DIR/$LOAD_POLICY/$NAME"
+    trash-put "$PACK_DIR/$LOAD_POLICY/$NAME"
+    git commit -m "remove plugin: $NAME"
+}
+
+upgrade ()
+{
+    set -x
+    git submodule update --remote --merge
+    git commit
+}
+
+case "$COMMAND" in
+    add|install )
+        add "$@" ;;
+    rm|remove )
+        remove "$@" ;;
+    update|upgrade )
+        upgrade "$@" ;;
+    help|"" )
+        help ;;
+    * )
+        help
+        printf "\nUnknown command: '%s'.\n" "$COMMAND" 1>&2
+esac
